f97cd3ab6bc69999b87ff209465d5f8b5c3f1d12
Camila Riveron <camila@isl.co>
2015-11-20 14:57:36 -0500

    #3 protractor test

--- test/specs/loginPage.js
+++ test/specs/loginPage.js
@@ -1,6 +1,22 @@
 describe('franklin login page', function() {
+
+  //TODO: check if the user is logged
   it('should be in the login page', function() {
     browser.get('http://localhost:3000');
+
+    expect(browser.getTitle()).toEqual('franklin-dashboard');
+
+    expect(element(by.id('franklin-login-button')).isPresent()).toBe(true);
+
+    var button = element(by.id('franklin-login-button'));
+
+    expect(button.getText()).toEqual('Login with GitHub');
+
+  });  
+
+  //TODO: check if the user is logged
+  it('should redirect to login page', function() {
+    browser.get('http://localhost:3000/#/otherpage');
 
     expect(browser.getTitle()).toEqual('franklin-dashboard');
 
